432524ef3a5bed34b976ff1e3cfc42eb0b708693
D-Raff <damonraffels@gmail.com>
2024-01-09 16:31:28 +0200

    fetching json data and moving reactor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <div class="info" >
+      <p class="lead" v-for="work in jobTitle" :key="work.title">
+        {{ work.title }}
+      </p>
+    </div>
     <div class="reactor-div">
       <ArcReactor class="reactor"/>
     </div>
@@ -15,22 +20,48 @@ export default {
   components: {
     ArcReactor
 
+  },
+  computed: {
+    // computed is for data that will change
+    jobTitle(){
+      return this.$store.state.jobTitle
+    }
+  },
+  mounted(){
+    // when this is placed on the dom
+    this.$store.dispatch('fetchjobTitle')
   }
 }
 </script>
 
 <style>
+.home{
+  display: flex;
+  justify-content: center;
+}
 .reactor-div{
   animation: fade-in 2s ease-in;
-  height: 100vh;
-  position: relative;
+  height: 700px;
+  width: fit-content;
+  position: absolute;
   justify-content: center;
   align-items: center;
   display: flex;
 }
 .reactor {
   animation: shrink 3s ease 3s forwards;
-  
+  height: 100%;
+}
+.info{
+  border: 3px solid blue;
+  height: 100px;
+  width: 200px;
+  position: relative;
+}
+.lead{
+  color: aliceblue;
+  position: relative;
+  z-index: 1;
 }
 
 @keyframes fade-in {
