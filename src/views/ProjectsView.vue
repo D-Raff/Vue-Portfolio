bd11958b4677e13e20368d002aca2fbc1d6f1a09
D-Raff <damonraffels@gmail.com>
2024-01-12 11:54:19 +0200

    added photos to projects

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,10 +1,10 @@
-<template>     
+<template>
     <div class="projects">
         <div class="row" v-if="projects">
             <div class="proj-card" v-for="project in projects" :key="project.name">
-                <div class="p-photo">
-                    <img src="" alt="">
-                </div>
+
+                <img class="p-photo" :src="project.image" alt="project-img">
+
                 <div class="p-name">
                     {{ project.name }}
                 </div>
@@ -34,19 +34,29 @@ export default {
 
 
 <style>
-    .projects{
-        border: 3px solid  blue;
-        min-height: 80vh;
-        width: 100%;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        align-items: center;
-    }
-    .proj-card{
-        border: 2px solid red;
-    }
+.projects {
+    border: 3px solid blue;
+    min-height: 80vh;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+}
 
+.proj-card {
+    border: 2px solid red;
+    border-radius: 10px;
+    width: 250px !important;
+    height: 300px;
+}
 
+.p-photo {
+    width: 100%;
+}
 
+img [alt='project-img'] {
+    width: 250px;
+    display: none;
+}
 </style>
