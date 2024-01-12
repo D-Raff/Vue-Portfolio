d598d053c46f12795127e017bca821c84d5d1876
D-Raff <damonraffels@gmail.com>
2024-01-12 12:12:15 +0200

    adding css to projects

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,13 +3,19 @@
         <div class="row" v-if="projects">
             <div class="proj-card" v-for="project in projects" :key="project.name">
 
-                <img class="p-photo" :src="project.image" alt="project-img">
-
-                <div class="p-name">
-                    {{ project.name }}
-                </div>
-                <div class="description">
-                    {{ project.description }}
+                <div class="p-body">
+                    <img class="p-photo" :src="project.image" alt="project-img">
+    
+                    <div class="p-name">
+                        {{ project.name }}
+                    </div>
+                    <div class="description">
+                        {{ project.description }}
+                    </div>
+                    <div class="links">
+                        <a :href="project.gitHub">dtd</a>
+                        <a :href="project.netlify">ft</a>
+                    </div>
                 </div>
 
 
@@ -45,18 +51,44 @@ export default {
 }
 
 .proj-card {
-    border: 2px solid red;
-    border-radius: 10px;
     width: 250px !important;
     height: 300px;
+    padding: 0 !important;
+}
+.p-body{
+    border: 3px solid #AA0505;
+    border-radius: 10px;
+    height: 100%;
+    width: 100%;
+    padding: 10px;
 }
 
 .p-photo {
     width: 100%;
+    height: 120px;
+    object-fit: cover;
 }
 
+.p-name {
+    font-family: iceland;
+    font-size: 30px;
+    color: #67C7EB;
+    text-shadow: 0px 0px 3px #7bc9e6;
+}
+.description{
+    font-size: 15px;
+    color: #B97D10;
+}
 img [alt='project-img'] {
     width: 250px;
     display: none;
+}
+.links{
+    border: 3px solid #AA0505;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
 }
 </style>
