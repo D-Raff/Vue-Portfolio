84b627e6efa14134cc94871ea41d8a0fe668e686
D-Raff <damonraffels@gmail.com>
2024-01-12 17:29:50 +0200

    updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
     <img src="https://i.ibb.co/5k1CD6K/Clipped-image-20240109-121729.png" alt="profilePhoto">
-    <div class="info" >
-    <p class="Name">I Am Damon Raffels</p>
-    <p v-if="jobTitle" class="titles">Aspiring <span class="lead" v-for="work in jobTitle" :key="work.title">
-        {{ work.title }}
-      </span></p>
-      
+    <div class="info">
+      <p class="Name">I Am Damon Raffels</p>
+      <p v-if="jobTitle" class="titles">Aspiring <span class="lead" v-for="work in jobTitle" :key="work.title">
+          {{ work.title }}
+        </span></p>
+
     </div>
     <div class="reactor-div">
-      <ArcReactor class="reactor"/>
+      <ArcReactor class="reactor" />
     </div>
   </div>
 </template>
@@ -26,11 +26,11 @@ export default {
   },
   computed: {
     // computed is for data that will change
-    jobTitle(){
+    jobTitle() {
       return this.$store.state.jobTitle
     }
   },
-  mounted(){
+  mounted() {
     // when this is placed on the dom
     this.$store.dispatch('fetchjobTitle')
   }
@@ -38,25 +38,31 @@ export default {
 </script>
 
 <style >
-.home{
+.home {
   display: flex;
   justify-content: center;
   background: black;
+  min-height: 100%;
+  width: 100vw;
 }
-.reactor-div{
+
+.reactor-div {
   animation: fade-in 2s ease-in;
   height: 600px;
+  width: 100%;
   width: fit-content;
   position: absolute;
   justify-content: center;
   align-items: center;
   display: flex;
 }
+
 .reactor {
   animation: shrink 3s ease 3s forwards;
   height: 100%;
 }
-.info{
+
+.info {
   height: 100px;
   width: 40%;
   position: relative;
@@ -70,24 +76,27 @@ export default {
   visibility: hidden;
   animation: fade 2s ease-in 5s 1 forwards;
 }
-.Name{
+
+.Name {
   font-size: 50px;
   margin: 0;
 }
-.titles{
+
+.titles {
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 5px;
 }
-.lead{
+
+.lead {
   color: aliceblue;
   position: relative;
   z-index: 1;
   color: #AA0505;
 }
 
-img[alt='profilePhoto']{
+img[alt='profilePhoto'] {
   height: 500px;
   position: absolute;
   left: 10%;
@@ -101,11 +110,13 @@ img[alt='profilePhoto']{
     opacity: 50%;
   }
 }
+
 @keyframes fade {
   from {
     opacity: 0%;
   }
-  to{
+
+  to {
     visibility: visible;
   }
 }
@@ -117,12 +128,55 @@ img[alt='profilePhoto']{
     bottom: 0;
     left: 0;
   }
-  
+
   to {
     transform: translateX(100px);
     height: 65%;
     aspect-ratio: 1/1;
     left: 15%;
+  }
+}
+
+@media screen and (max-width:670px) {
+  .reactor-div {
+    width: 100%;
+  }
+  .home{
+    height: 800px;
+  }
+  .info{
+    top: 500px;
+    left: 100px;
+  }
+
+  .reactor {
+    height: 50%;
+    bottom: 20px;
+  }
+
+  img[alt='profilePhoto'] {
+    width: 300px;
+    height: 400px;
+    object-fit: cover;
+    position: absolute;
+    visibility: hidden;
+    animation: fade 2s ease-in 6s 1 forwards;
+    top: 350px;
+  }
+
+  @keyframes shrink {
+    from {
+      transform: translateX(0px);
+      bottom: 0;
+      left: 0;
+    }
+    
+    to {
+      transform: translateX(0px);
+      bottom: 0;
+      left: 0;
+      aspect-ratio: 1/1;
+    }
   }
 }
 </style>
